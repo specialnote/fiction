9cde942dfa550c0e4aedb5965649f8074dade294
shiyang <2022281825@qq.com>
2016-07-28 22:36:27 +0800

    添加日志

---
+++ console/migrations/m160728_141436_log.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Migration;
+
+class m160728_141436_log extends Migration
+{
+    public function up()
+    {
+        $this->createTable('log', [
+            'id' => $this->primaryKey(),
+            'type' => $this->integer(1),
+            'app' => $this->string(20),
+            'controller' => $this->string(20),
+            'action' => $this->string(20),
+            'model' => $this->string(20),
+            'function' => $this->string(20),
+            'work' => $this->string(100),
+            'note' => $this->string(100),
+            'created_at' => $this->timestamp(),
+            'updated_at' => $this->timestamp(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('log');
+    }
+}
