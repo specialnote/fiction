8969002aba97be811d391b7a54bdc425a6f306e2
shiyang <shiyang@wangcaigu.com>
2016-07-29 18:34:05 +0800

    邮件

---
+++ console/controllers/MailController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace console\controllers;
+
+
+use yii\console\Controller;
+use Yii;
+
+class MailController extends Controller
+{
+    public function actionTest()
+    {
+        $mail = Yii::$app->mailer->compose();
+        $mail = Yii::$app->mailer->compose(['html' => 'ceshi'], ['key' => '测试模板']);
+        $mail->setTo('shiyang@wangcaigu.com');
+        $mail->setSubject('测试Yii2邮件发送');
+        $mail->setHtmlBody('邮件内容，这里可以使用 HTML 代码');
+        $mail->send();//发送
+    }
+}
