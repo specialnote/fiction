45172dae09b939f8e2145d452f520af8b2fe0604
shiyang <shiyang@wangcaigu.com>
2016-08-31 13:35:02 +0800

    添加百度年龄统计

--- frontend/web/js/baidu_count.js
+++ frontend/web/js/baidu_count.js
@@ -4,4 +4,6 @@ var _hmt = _hmt || [];
     hm.src = "//hm.baidu.com/hm.js?acf9926b564fbd1dac04164fe70c6643";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
+    hm.src = "//hm.baidu.com/hm.js?b271b4f208b50c3b8abe71cabdab9916";
+    s.parentNode.insertBefore(hm, s);
 })();
