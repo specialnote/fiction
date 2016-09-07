16db170665e0b2563badaf88f9087f1d73ae8818
shiyang <shiyang@wangcaigu.com>
2016-09-07 15:59:34 +0800

    添加sitemap

--- console/controllers/BaiduController.php
+++ console/controllers/BaiduController.php
@@ -9,6 +9,7 @@ use yii\console\Controller;
 
 class BaiduController extends Controller
 {
+    //向百度推送链接
     public function actionLinkPush()
     {
         $urls = Fiction::getAllFictionUrl();
@@ -18,5 +19,22 @@ class BaiduController extends Controller
             $sliceUrls = array_slice($urls, ($i - 1) * 1000, 1000);
             $res = LinkPush::push($sliceUrls);
         }
+    }
+
+    //生成百度sitemap
+    public function actionSitemap()
+    {
+        $mapString = <<<STRING
+<?xml version="1.0" encoding="UTF-8" ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+xmlns:mobile="http://www.baidu.com/schemas/sitemap-mobile/1/">
+STRING;
+        $urls = Fiction::getAllFictionUrl();
+        foreach ($urls as $url) {
+            $date = date('Y-m-d');
+            $mapString = $mapString . "<url>\n<loc>$url</loc>\n<mobile:mobile type=\"pc,mobile\"/>\n<lastmod>$date</lastmod>\n<changefreq>daily</changefreq>\n<priority>0.8</priority>\n</url>\n";
+        }
+        $mapString = $mapString . "</urlset>";
+        file_put_contents(__DIR__ . "/../../frontend/web/sitemap_main.xml", $mapString);
     }
 }
